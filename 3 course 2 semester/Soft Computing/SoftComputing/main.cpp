b52c9307c85371b7790ddb3920b17ef451c84286
Ikhtiyor Rakhimov <ihtier_@outlook.com>
2021-05-16 22:10:28 +0300

    16.05.2021

--- 3 course 2 semester/Soft Computing/SoftComputing/main.cpp
+++ 3 course 2 semester/Soft Computing/SoftComputing/main.cpp
@@ -4,6 +4,7 @@
 #include <QVector2D>
 #include <functional>
 #include <QMap>
+#include <algorithm>
 
 #include <QDebug>
 #include <iostream>
@@ -201,6 +202,140 @@ void printMatrix(const QVector<QVector<qreal>> &matrix)
     }
 }
 
+QVector<QVector<qreal>> Union(const QVector<QVector<qreal>>& left,
+                              const QVector<QVector<qreal>>& right)
+{
+    qint32 size= left.size();
+
+    QVector<QVector<qreal>> res(size, QVector<qreal>(size));
+
+    for(qint32 i = 0; i < size; ++i)
+    {
+        for(qint32 j = 0; j < size; ++j)
+        {
+            res[i][j] = qMax(left[i][j], right[i][j]);
+        }
+    }
+
+    return res;
+}
+
+bool isEqual(const QVector<QVector<qreal>>& left,
+             const QVector<QVector<qreal>>& right)
+{
+    qint32 size= left.size();
+
+    for(qint32 i = 0; i < size; ++i)
+    {
+        for(qint32 j = 0; j < size; ++j)
+        {
+            if(left[i][j] != right[i][j])
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+QVector<QVector<qreal>> composition(const QVector<QVector<qreal>>& left,
+                                    const QVector<QVector<qreal>>& right,
+                                    const std::function<qreal(const qreal&, const qreal&)> &T
+                                    = [](const qreal& left, const qreal& right) -> qreal
+                                    {
+                                        return qMin(left,right);
+                                    })
+{
+    qint32 size= left.size();
+
+    QVector<qreal> tmp(size);
+    QVector<QVector<qreal>> res(size, tmp);
+
+    for(qint32 i = 0; i < size; ++i)
+    {
+        for(qint32 j = 0; j < size; ++j)
+        {
+            for(qint32 k = 0; k < size; ++k)
+            {
+                tmp[k] = T(left[i][k],right[k][j]);
+            }
+
+            res[i][j] = *std::max_element(tmp.begin(), tmp.end());
+        }
+    }
+
+    return res;
+}
+
+QVector<QVector<qreal>> transitiveClosure(const QVector<QVector<qreal>>& matrix,
+                                          const std::function<qreal(const qreal&, const qreal&)> &T
+                                          = [](const qreal& left, const qreal& right) -> qreal
+                                          {
+                                              return qMin(left,right);
+                                          })
+{
+    QVector<QVector<qreal>> Un = matrix, Akprev, Ak = matrix;
+
+    qint32 km1 = 1;
+
+    do
+    {
+        Akprev = Ak; //A[k-1]
+        Ak = composition(Akprev, matrix, T); // A[k+1]
+        Un = Union(Un, Ak);
+        ++km1;
+    }while (!isEqual(Ak, Akprev));
+
+    qDebug() << "Number of iterations:" << km1;
+
+    return Un;
+}
+
+void print1IfGreatOrEqualThenValueElse0(const QVector<QVector<qreal>> &matrix,
+                                        const qreal& value)
+{
+    qint32 size = matrix.size();
+
+    for(qint32 i = 0; i < size; ++i)
+    {
+        for(qint32 j = 0; j < size; ++j)
+        {
+            if(matrix[i][j] >= value)
+            {
+                std::cout << 1;
+            }
+            else
+            {
+                std::cout << 0;
+            }
+        }
+        std::cout << '\n';
+    }
+}
+
+void uniqueValues(const QVector<QVector<qreal>> &matrix)
+{
+    QVector<qreal> res;
+
+    qint32 size = matrix.size();
+
+    for(qint32 i = 0; i < size; ++i)
+    {
+        for(qint32 j = 0; j < size; ++j)
+        {
+            if(res.indexOf(matrix[i][j]) == -1)
+            {
+                res.push_back(matrix[i][j]);
+            }
+        }
+    }
+
+    std::sort(res.begin(), res.end());
+
+    qDebug() << res;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -218,8 +353,19 @@ int main(int argc, char *argv[])
     printMatrix(dissimilarityMatrix(Aabcde));
     qDebug() << '\n';
     printMatrix(milarityMatrix(dissimilarityMatrix(Aabcde)));
+    qDebug() << '\n';
 
+    std::function<qreal(const qreal&, const qreal&)> T = [](const qreal& x, const qreal& y) -> qreal
+    {
+        qreal a = 0.5, b = -1;
+        return qMax(0.,(x*y-(1-a)*(1-b)*(1-x)*(1-y))/(1+a*b*(1-x)*(1-y)));
+    };
 
+    printMatrix(transitiveClosure(milarityMatrix(dissimilarityMatrix(Aabcde)),T));
+
+    uniqueValues(transitiveClosure(milarityMatrix(dissimilarityMatrix(Aabcde)),T));
+
+    print1IfGreatOrEqualThenValueElse0(transitiveClosure(milarityMatrix(dissimilarityMatrix(Aabcde)),T), 0.64);
 
     return a.exec();
 }
